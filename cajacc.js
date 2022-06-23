ae65f36c84aa8ab26a4f4b085508c8a2d1d3fff9
PDMA1979 <pedrodomingomurillo1979@gmail.com>
2022-06-23 17:44:17 -0400

    first commit

---
+++ cajacc.js
@@ -0,0 +1,23 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector(`.agregar-imagen`)
+const contenedorLigth = document.querySelector('.imagen-a')
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src')) 
+    })
+})
+
+
+contenedorLigth.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight)
+    contenedorLigth.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    hamburguer1.style.opacity = '1'
+})
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen;
+    contenedorLigth.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    hamburguer1.style.opacity = '0' 
+}
